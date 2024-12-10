abfc90f62f76f6dd78271559ff349ab70e3f0706
Moarbue <83272310+Moarbue@users.noreply.github.com>
2024-12-10 19:36:46 +0100

    Update main example

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "camera.h"
 #include "cube.h"
 #include "logging.h"
 #include "smath.h"
@@ -8,8 +9,9 @@ void window_size_callback(int width, int height);
 
 void render_frame(void);
 
-Mat4 view, proj, view_proj;
+Mat4 proj, view_proj;
 Rubiks_Cube *rc;
+Camera *cam;
 
 int main(void)
 {
@@ -40,14 +42,17 @@ int main(void)
 
     rc = rubiks_cube(&rcconf);
 
-    // view = mat4_look_at(vec3_from_spherical(3.0f, rad(0.0f), rad(0.0f)), rcconf.origin, vec3(0.0f, 1.0f, 0.0f));
-    view = mat4_look_at(vec3(0, 0, 3), rcconf.origin, vec3(0.0f, 1.0f, 0.0f));
+    cam = camera(rcconf.origin, 3, rad(45), rad(0), rad(0));
+    camera_set_animation_duration(cam, CAM_ANIM_ALL, 0.5f);
+    camera_set_animation_easing_func(cam, CAM_ANIM_ALL, ease_in_out_sine);
+
     proj = mat4_perspective(rad(45.0f), window_get_aspect_ratio(), 0.01f, 100.0f);
-    view_proj = mat4_mul(proj, view);
+    view_proj = mat4_mul(proj, camera_get_view_matrix(cam));
 
     window_main_loop(render_frame);
 
-    rubiks_cube_delete(rc);
+    rubiks_cube_free(rc);
+    camera_free(cam);
     window_close();
     return 0;
 }
@@ -62,16 +67,41 @@ void key_callback(int key, int action, int mods)
     if (key == KEY_F11 && action == KEY_PRESS) {
         window_toggle_fullscreen();
     }
+
+    // Rotate +-90 degrees about X-Axis
+    if (key == KEY_1 && action == KEY_PRESS) {
+        camera_add_to_orbit_position(cam, 0.0f,  M_PI_2, 0.0f, 0.0f);
+    }
+    if (key == KEY_2 && action == KEY_PRESS) {
+        camera_add_to_orbit_position(cam, 0.0f, -M_PI_2, 0.0f, 0.0f);
+    }
+    // Rotate +-90 degrees about Y-Axis
+    if (key == KEY_3 && action == KEY_PRESS) {
+        camera_add_to_orbit_position(cam, 0.0f, 0.0f,  M_PI_2, 0.0f);
+    }
+    if (key == KEY_4 && action == KEY_PRESS) {
+        camera_add_to_orbit_position(cam, 0.0f, 0.0f, -M_PI_2, 0.0f);
+    }
+    // Rotate +-90 degrees about Z-Axis
+    if (key == KEY_5 && action == KEY_PRESS) {
+        camera_add_to_orbit_position(cam, 0.0f, 0.0f, 0.0f,  M_PI_2);
+    }
+    if (key == KEY_6 && action == KEY_PRESS) {
+        camera_add_to_orbit_position(cam, 0.0f, 0.0f, 0.0f, -M_PI_2);
+    }
 }
 
 void window_size_callback(int width, int height)
 {
     mat4_perspective_resize(&proj, (float)width / (float) height);
-    view_proj = mat4_mul(proj, view);
+    view_proj = mat4_mul(proj, camera_get_view_matrix(cam));
 }
 
 void render_frame(void)
 {
+    camera_update(cam, window_get_frame_time());
+    view_proj = mat4_mul(proj, camera_get_view_matrix(cam));
+
     window_clear(color_from_hex(0xDFD3C3FF));
     rubiks_cube_draw(rc, view_proj);
 }
